be04818e260a2d923183d6d80036d431a651e563
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-02-08 08:25:26 +0200

    Simplify the regex in snmpd_exec.pp
    Before, the regex failed if the command contained single quotes.
    
    Now the regex only seaches for the string "exec $job_name" in the
    snmpd.conf file.

--- manifests/snmpd_exec.pp
+++ manifests/snmpd_exec.pp
@@ -8,8 +8,8 @@ define snmp_rhel::snmpd_exec($job_name = $title, $command) {
   $snmp_exec = "exec $job_name $command"
 
   # Append an exec statement into the snmpd.conf file, if it is not yet there
-  exec { "echo '$snmp_exec' >> /etc/snmp/snmpd.conf":
-    unless => "cat /etc/snmp/snmpd.conf | grep -e '^$snmp_exec'",
+  exec { "echo $snmp_exec >> /etc/snmp/snmpd.conf":
+    unless => "cat /etc/snmp/snmpd.conf | grep -e '^exec $job_name'",
     notify => Service['snmpd']
   }
 }
