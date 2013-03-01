05eefbf96840b893e8b2ec44eeaeb8cddad206cc
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2013-03-01 14:30:33 +0200

    Fix exec regex

--- manifests/snmpd_exec.pp
+++ manifests/snmpd_exec.pp
@@ -9,7 +9,7 @@ define snmp_rhel::snmpd_exec($job_name = $title, $command) {
 
   # Append an exec statement into the snmpd.conf file, if it is not yet there
   exec { "echo $snmp_exec >> /etc/snmp/snmpd.conf":
-    unless => "cat /etc/snmp/snmpd.conf | grep -e '^exec $job_name'",
+    unless => "cat /etc/snmp/snmpd.conf | grep -e '^exec $job_name '",
     notify => Service['snmpd']
   }
 }
